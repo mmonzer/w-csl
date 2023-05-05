621ee54d2859a16f78c9c8fc57e53f78f29da171
Mohamad Monzer <mohamad.monzer@aphelio.fr>
2023-05-05 15:48:45 +0200

    add a create certificate store script to facilitate dev env

---
+++ create_certificate_store.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CERT_FILE=../csl-docker/config/nginx/certs/csl_hmi.crt
+CACERT=cacerts.jks
+PASS=changeit
+CERT_ALIAS=CSL_HMI
+
+rm $CACERT
+
+echo "yes" | keytool -import -v -trustcacerts \
+-alias $CERT_ALIAS -file $CERT_FILE \
+-keystore $CACERT -keypass $PASS \
+-storepass $PASS

69884d54e588a6dd3d400989804d6debcb8c036e
Clément BERAT <clement.berat@aphelio.fr>
2023-12-12 10:43:57 +0100

    Add 'isRequired' field to inputs

--- src/com/csl/intercom/cslscan/models/EntityHttpConnectionInput.java
+++ src/com/csl/intercom/cslscan/models/EntityHttpConnectionInput.java
@@ -10,6 +10,7 @@ public class EntityHttpConnectionInput {
     private String key;
     private String defaultValue;
     private boolean isSecret;
+    private boolean isRequired;
     private Map<Language, String> description;
     private Map<Language, String> tooltip;
     private Map<Language, String> placeholder;
@@ -64,6 +65,11 @@ public class EntityHttpConnectionInput {
         } else {
             entityHttpConnectionInput.isSecret = false;
         }
+        if (json.has(EntityHttpConnectionInputField.REQUIRED.dbapiName()) && json.get(EntityHttpConnectionInputField.REQUIRED.dbapiName()).isBoolean()) {
+            entityHttpConnectionInput.isRequired = json.get(EntityHttpConnectionInputField.REQUIRED.dbapiName()).asBoolean();
+        } else {
+            entityHttpConnectionInput.isRequired = false;
+        }
         return entityHttpConnectionInput;
     }
 
@@ -91,6 +97,11 @@ public class EntityHttpConnectionInput {
             entityHttpConnectionInput.isSecret = json.get(EntityHttpConnectionInputField.SECRET.scanName()).asBoolean();
         } else {
             entityHttpConnectionInput.isSecret = false;
+        }
+        if (json.has(EntityHttpConnectionInputField.REQUIRED.scanName()) && json.get(EntityHttpConnectionInputField.REQUIRED.scanName()).isBoolean()) {
+            entityHttpConnectionInput.isRequired = json.get(EntityHttpConnectionInputField.REQUIRED.scanName()).asBoolean();
+        } else {
+            entityHttpConnectionInput.isRequired = false;
         }
         return entityHttpConnectionInput;
     }
@@ -124,6 +135,7 @@ public class EntityHttpConnectionInput {
             serialized.set(EntityHttpConnectionInputField.DISPLAY.dbapiName(), displaySerialized);
         }
         serialized.set(EntityHttpConnectionInputField.SECRET.dbapiName(), isSecret);
+        serialized.set(EntityHttpConnectionInputField.REQUIRED.dbapiName(), isRequired);
         return serialized;
     }
 
@@ -156,6 +168,7 @@ public class EntityHttpConnectionInput {
             serialized.set(EntityHttpConnectionInputField.DISPLAY.scanName(), displaySerialized);
         }
         serialized.set(EntityHttpConnectionInputField.SECRET.scanName(), isSecret);
+        serialized.set(EntityHttpConnectionInputField.REQUIRED.scanName(), isRequired);
         return serialized;
     }
 

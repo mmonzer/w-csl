4b288c986fe8cc0404f2e803cfd109131dfcb79f
Clément BERAT <clement.berat@aphelio.fr>
2024-02-21 17:10:20 +0100

    Add support for SSH connections

---
+++ src/com/csl/intercom/dbapi/enums/SshConnectionField.java
@@ -0,0 +1,35 @@
+package com.csl.intercom.dbapi.enums;
+
+public enum SshConnectionField {
+    PORT("port", "port"),
+    USERNAME("username", "username"),
+    PASSWORD("read_only_password", "password"),
+    PRIVATE_KEY("private_key", "privateKey"),
+    PASSPHRASE("passphrase", "passphrase"),
+    ;
+
+    private String dbapiName;
+    private String scanName;
+
+    SshConnectionField(String dbapiName, String scanName) {
+        this.dbapiName = dbapiName;
+        this.scanName = scanName;
+    }
+
+    public String dbapiName() {
+        return dbapiName;
+    }
+
+    public String scanName() {
+        return scanName;
+    }
+
+    public static SshConnectionField fromDbapiName(String dbapiName) {
+        for (SshConnectionField field : values()) {
+            if (field.dbapiName.equals(dbapiName)) {
+                return field;
+            }
+        }
+        return null;
+    }
+}

862d33dfe986b3b1b89d89d13e29cb3ca2ce72a1
Pablo C <pablo.campos@aphelio.fr>
2024-07-22 14:29:41 +0200

    Factorize dbapi handler for alerts

---
+++ src/main/java/com/csl/alert/DbapiHandlerForAlerts.java
@@ -0,0 +1,45 @@
+package com.csl.alert;
+
+import com.csl.core.CSLContext;
+import com.csl.intercom.dbapi.DbapiHandler;
+import com.ucsl.interfaces.IAlertDescriptor;
+import com.ucsl.json.Json;
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.client.api.Request;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Manage HTTP communications with DB-API.
+ * Provides an interface for retrieving the devices, connections and so on,
+ * and to send information to it (CPE Items, a Scan's status, ...).
+ */
+public class DbapiHandlerForAlerts extends DbapiHandler {
+    private static final Logger logger = LoggerFactory.getLogger(DbapiHandler.class);
+
+    public DbapiHandlerForAlerts() {
+        this("CSLAlerts", CSLContext.instance.getConfig());
+    }
+
+    public DbapiHandlerForAlerts(String moduleName, Json config) {
+        super(moduleName, config);
+    }
+
+    /**
+     * Insert a new alert into DB
+     *
+     * @param alert alert to insert
+     * @return the content of the response
+     */
+    public String insertAlert(IAlertDescriptor alert) {
+        String res = null;
+        Request request = createRequest("POST", "/alerts", null, alert.toJson());
+        try {
+            ContentResponse response = request.send();
+            res = new String(response.getContent());
+        } catch (Exception e) {
+            logger.error("Could not send alerts to DB-API.", e);
+        }
+        return res;
+    }
+}

af48f902dfd107189c319e1b7ee6f8abf305533c
Hussein Fawaz <hussein.fawaz@aphelio.fr>
2024-12-23 17:19:14 +0100

    send autocrpyt command to add them in dbapi permission

--- src/main/java/com/csl/intercom/jsoncmd/JsonCmdPrivilegeFamily.java
+++ src/main/java/com/csl/intercom/jsoncmd/JsonCmdPrivilegeFamily.java
@@ -13,6 +13,9 @@ public enum JsonCmdPrivilegeFamily {
     READ_CONNECTION_CERTIFICATE("read_connection_certificate"),
     UPDATE_CONNECTION_CERTIFICATE("update_connection_certificate"),
     DELETE_CONNECTION_CERTIFICATE("delete_connection_certificate"),
+    MANAGE_CERTIFICATE_AUTHORITY("manage_certificate_authority"),
+    MANAGE_ROLE("manage_role"),
+    MANAGE_CERTIFICATE("manage_certificate"),
     ;
 
     private final String privilegeFamily;
